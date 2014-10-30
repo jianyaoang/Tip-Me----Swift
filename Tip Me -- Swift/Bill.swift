bfe7cc7d305b24e91d0b30fb3b831bde91b6ebaa
jianyaoang <jianyaoang1@gmail.com>
2014-10-30 13:03:17 +0800

    Enable calculation of ten percent tip

---
+++ Tip Me -- Swift/Bill.swift
@@ -0,0 +1,16 @@
+//
+//  Bill.swift
+//  Tip Me -- Swift
+//
+//  Created by Jian Yao Ang on 10/30/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+import Foundation
+
+class Bill: NSObject
+{
+    var totalBillAmount: NSString?
+    var theTotalBillAmount: Double?
+    var numberToBePaidPerPerson: Double?
+}
--- Tip Me -- Swift/ViewController.swift
+++ Tip Me -- Swift/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet var numberOfPeopleTextField: UITextField!
     @IBOutlet var tenPercent: UIButton!
     
+    var totalBillAmount: Double?
+    
+    var bill = Bill()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,7 +27,37 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
 
     }
-
+    
+    @IBAction func onTenPercentButtonPressed(sender: AnyObject)
+    {
+        self.totalBillTextField.resignFirstResponder()
+        
+        var billAmountValue = self.totalBillTextField.text
+        var billAmountDoubleValue : Double = NSString(string: billAmountValue).doubleValue
+        
+        var tenPercentOfBillAmount = 0.1 * billAmountDoubleValue
+        
+        self.totalBillAmount = billAmountDoubleValue + tenPercentOfBillAmount
+        
+        println("this is self.totalBillAmount = \(self.totalBillAmount)")
+    }
+    
+    @IBAction func onCalculateButtonPressed(sender: AnyObject)
+    {
+        var numberOfPeopleString = self.numberOfPeopleTextField.text
+        var numberOfPeopleDoubleValue : Double = NSString(string: numberOfPeopleString).doubleValue
+        
+        var billToBePaidPerPerson = self.totalBillAmount! / numberOfPeopleDoubleValue
+        
+        self.bill.numberToBePaidPerPerson = billToBePaidPerPerson
+        
+        println("self.bill.numberToBePaidPerPerson = \(self.bill.numberToBePaidPerPerson)")
+    }
+    
+    override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!)
+    {
+        self.view.endEditing(true)
+    }
 
 }
 
